b2723546fdfc85dcbdbccc05bbb4012ae9483396
coolabhays <abhaysp9955@gmail.com>
2020-10-28 21:23:51 +0530

    ayu theme

--- config.h
+++ config.h
@@ -3,24 +3,24 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 550;                    /* minimum width when centered */
+static int min_width = 750;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Monaco:size=12"
+	"Source Code Pro:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#b9b9b9", "#101010" },
-	[SchemeSel] = { "#b9b9b9", "#525252" },
-	[SchemeSelHighlight] = { "#3c3836", "#686868" },
-	[SchemeNormHighlight] = { "#868686", "#3c3836" },
-	[SchemeOut] = { "#101010", "#686868" },
-	[SchemeMid] = { "#333", "#8e8e8e" },
+	[SchemeNorm] = { "#b3b1ad", "#0a0e14" },
+	[SchemeSel] = { "#b3b1ad", "#626a73" },
+	[SchemeSelHighlight] = { "#ffc978", "#39bae6" },
+	[SchemeNormHighlight] = { "#95e6cb", "#222222" },
+	[SchemeOut] = { "#0a0e14", "#59c2ff" },
+	[SchemeMid] = { "#eeeeee", "#c2d94c" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 0; // -h option; minimum height of a menu line
+static unsigned int lineheight = 0;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
