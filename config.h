e40bc08d23bc4cc9fa9bf8c4778af8eb56003782
coolabhays <abhaysp9955@gmail.com>
2021-01-20 09:16:03 +0530

    updated colorschemes

--- config.h
+++ config.h
@@ -6,16 +6,16 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 750;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Source Code Pro:size=10"
+	"Fira Code Retina:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#c5cdd9", "#2b2d3a" },
+	[SchemeSel] = { "#2b2d3a", "#5dbbc1" },
+	[SchemeSelHighlight] = { "#ec7279", "#5dbbc1" },
+	[SchemeNormHighlight] = { "#deb974", "#363a4e" },
+	[SchemeOut] = { "#363a4e", "#6cb6eb" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
