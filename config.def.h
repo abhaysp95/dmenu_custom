2a92311eb213eae2da381915edf42574fa7c5cd4
coolabhays <abhaysp9955@gmail.com>
2021-01-21 10:43:58 +0530

    successfully applied border patch

--- config.def.h
+++ config.def.h
@@ -26,3 +26,6 @@ static unsigned int lineheight = 0;         /* -h option; minimum height of a me
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 2;
